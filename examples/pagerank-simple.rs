49973eaf4739860687651c0ac8a6f6d0f5da1d91
Frank McSherry <fmcsherry@me.com>
2015-06-09 17:42:26 +0100

    pagerank examples

---
+++ examples/pagerank-simple.rs
@@ -0,0 +1,120 @@
+#![feature(scoped)]
+#![feature(collections)]
+
+extern crate mmap;
+extern crate time;
+extern crate timely;
+extern crate columnar;
+extern crate dataflow_join;
+
+extern crate docopt;
+use docopt::Docopt;
+
+use std::thread;
+
+use dataflow_join::graph::{GraphTrait, GraphMMap};
+
+use timely::progress::timestamp::RootTimestamp;
+use timely::progress::scope::Scope;
+use timely::progress::nested::Summary::Local;
+use timely::example_static::*;
+use timely::communication::*;
+use timely::communication::pact::Exchange;
+
+use timely::drain::DrainExt;
+
+static USAGE: &'static str = "
+Usage: pagerank <source> <workers>
+";
+
+fn main () {
+    let args = Docopt::new(USAGE).and_then(|dopt| dopt.parse()).unwrap_or_else(|e| e.exit());
+
+    let workers = if let Ok(threads) = args.get_str("<workers>").parse() { threads }
+                  else { panic!("invalid setting for workers: {}", args.get_str("<workers>")) };
+    println!("starting pagerank dataflow with {:?} worker{}", workers, if workers == 1 { "" } else { "s" });
+    let source = args.get_str("<source>");
+
+    pagerank_multi(ProcessCommunicator::new_vector(workers), |_, _| GraphMMap::new(&source));
+}
+
+fn pagerank_multi<C, G, F>(communicators: Vec<C>, loader: F)
+where C: Communicator+Send, G: GraphTrait<Target=u32>, F: Fn(u64, u64)->G+Send+Sync {
+    let mut guards = Vec::new();
+    let loader = &loader;
+    for communicator in communicators.into_iter() {
+        guards.push(thread::Builder::new().name(format!("timely worker {}", communicator.index()))
+                                          .scoped(move || pagerank(communicator, loader))
+                                          .unwrap());
+    }
+}
+
+fn pagerank<C, G, F>(communicator: C, loader: &F)
+where C: Communicator,
+      G: GraphTrait<Target=u32>,
+      F: Fn(u64, u64)->G {
+    let index = communicator.index() as usize;
+    let peers = communicator.peers() as usize;
+
+    let mut root = GraphRoot::new(communicator);
+
+    {   // new scope avoids long borrow on root
+        let mut builder = root.new_subgraph();
+
+        // establish the beginnings of a loop,
+        // 20 iterations, each time around += 1.
+        let (helper, stream) = builder.loop_variable::<(u32, f32)>(RootTimestamp::new(20), Local(1));
+
+        let graph = loader(index as u64, peers as u64);             // load up our graph data
+        let mut src = vec![1.0; graph.nodes() / peers as usize];    // local rank accumulation
+        let mut dst = vec![0.0; graph.nodes()];                     // local rank accumulation
+
+        let mut start = time::precise_time_s();
+
+        // from feedback, place an operator that
+        // aggregates and broadcasts ranks along edges.
+        stream.enable(builder).unary_notify(
+
+            Exchange::new(|x: &(u32, f32)| x.0 as u64),     // 1. how data should be exchanged
+            format!("PageRank"),                            // 2. a tasteful, descriptive name
+            vec![RootTimestamp::new(0)],                    // 3. indicate an initial capability
+            move |input, output, iterator| {                // 4. provide the operator logic
+
+                while let Some((iter, _)) = iterator.next() {
+                    // /---- should look familiar! ----\
+                    for node in 0..src.len() {
+                        src[node] = 0.15 + 0.85 * src[node];
+                    }
+
+                    for node in 0..src.len() {
+                        let edges = graph.edges(index + peers * node);
+                        let value = src[node] / edges.len() as f32;
+                        for &b in edges {
+                            dst[b as usize] += value;
+                        }
+                    }
+                    // \------ end familiar part ------/
+                    output.give_at(&iter, dst.drain_temp()
+                                             .enumerate()
+                                             .filter(|&(_,f)| f != 0.0)
+                                             .map(|(u,f)| (u as u32, f)));
+
+                    dst.resize(graph.nodes(), 0.0);
+
+                    println!("{}s", time::precise_time_s() - start);
+                    start = time::precise_time_s();
+                }
+
+                while let Some((iter, data)) = input.pull() {
+                    iterator.notify_at(&iter);
+                    for (node, rank) in data.drain_temp() {
+                        src[node as usize / peers] += rank;
+                    }
+                }
+            }
+        )
+        .connect_loop(helper);
+    }
+
+    while root.step() { }
+}
---
+++ examples/pagerank-single.rs
@@ -0,0 +1,46 @@
+extern crate mmap;
+extern crate time;
+extern crate timely;
+extern crate columnar;
+extern crate dataflow_join;
+
+extern crate docopt;
+use docopt::Docopt;
+
+use dataflow_join::graph::{GraphTrait, GraphMMap};
+
+static USAGE: &'static str = "
+Usage: pagerank <source>
+";
+
+fn main () {
+    let args = Docopt::new(USAGE).and_then(|dopt| dopt.parse()).unwrap_or_else(|e| e.exit());
+    let source = args.get_str("<source>");
+
+    pagerank(&GraphMMap::new(&source), 20);
+}
+
+fn pagerank<G: GraphTrait<Target=u32>>(graph: &G, iterations: usize) {
+
+    let mut src = vec![1.0f32; graph.nodes()];
+    let mut dst = vec![0.0f32; graph.nodes()];
+
+    let mut start = time::precise_time_s();
+
+    for iteration in 0..iterations {
+        for node in 0..src.len() {
+            src[node] = 0.15 + 0.85 * src[node];
+        }
+        for node in 0..src.len() {
+            let edges = graph.edges(node);
+            let value = src[node] / edges.len() as f32;
+            for &edge in edges {
+                dst[edge as usize] += value;
+            }
+        }
+        for node in 0..src.len() { src[node] = dst[node]; dst[node] = 0; }
+
+        println!("{}s", time::precise_time_s() - start);
+        start = time::precise_time_s();
+    }
+}
