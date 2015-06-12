0c12b6704dbc19dfda08d2d276ffb6d3070cd5e7
Frank McSherry <fmcsherry@me.com>
2015-06-12 11:03:40 +0100

    updated transpose implementation

--- examples/pagerank-transpose.rs
+++ examples/pagerank-transpose.rs
@@ -126,7 +126,7 @@ fn transpose(filename: String, index: usize, peers: usize) -> (Vec<u32>, Vec<(u3
 
         let len = rev.len();
         rev[len-1].1 += 1;
-        reversed.push(s);
+        reversed.push(s / peers);
     }
 
     return (deg, rev, reversed);
